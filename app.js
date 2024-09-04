1c89bfe42d083499d005302aff785d48ca51cdab
Vajahath Ahmed <vajuoff.1@gmail.com>
2024-09-04 21:56:39 +0530

    feat(issue): added additional events for reopening, deleted

--- app.js
+++ app.js
@@ -12,28 +12,41 @@ export default (app) => {
   app.log.info("Yay! The app was loaded!");
 
   app.on("issues.opened", async (context) => {
-    const { title, number, html_url } = context.payload.issue;
+    await addNewIssue(context);
+  });
 
-    await appendRow(
-      sheets.sprintPlanningDoc.spreadsheetId,
-      sheets.sprintPlanningDoc.range,
-      [`=HYPERLINK("${html_url}", ${number})`, title]
-    );
+  app.on("issues.reopened", async (context) => {
+    await addNewIssue(context);
   });
 
   app.on("issues.closed", async (context) => {
-    const { number } = context.payload.issue;
-    const sheetIssueColumnValues = await getData(
-      sheets.sprintPlanningDoc.spreadsheetId
-    );
-    console.log("sheetIssueColumnValues", sheetIssueColumnValues);
-    const indexToRemove = sheetIssueColumnValues.findIndex(
-      (row) => row[0] === number
-    );
-    console.log("indexToRemove", indexToRemove);
-    await deleteSingleRow(
-      sheets.sprintPlanningDoc.spreadsheetId,
-      indexToRemove
-    );
+    await deleteIssue(context);
+  });
+
+  app.on("issues.deleted", async (context) => {
+    await deleteIssue(context);
   });
 };
+
+async function addNewIssue(context) {
+  const { title, number, html_url } = context.payload.issue;
+
+  await appendRow(
+    sheets.sprintPlanningDoc.spreadsheetId,
+    sheets.sprintPlanningDoc.range,
+    [`=HYPERLINK("${html_url}", ${number})`, title]
+  );
+}
+
+async function deleteIssue(context) {
+  const { number } = context.payload.issue;
+  const sheetIssueColumnValues = await getData(
+    sheets.sprintPlanningDoc.spreadsheetId
+  );
+  console.log("sheetIssueColumnValues", sheetIssueColumnValues);
+  const indexToRemove = sheetIssueColumnValues.findIndex(
+    (row) => row[0] === number
+  );
+  console.log("indexToRemove", indexToRemove);
+  await deleteSingleRow(sheets.sprintPlanningDoc.spreadsheetId, indexToRemove);
+}
